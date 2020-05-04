af2b1fa282a26fabc7d2801fe6a935053b41b222
Nastya Drugova <drugova723@gmail.com>
2020-05-04 17:49:41 +0300

    created a new Task 'minify-css'

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
 const gulp = require ('gulp');
-var browserSync = require('browser-sync').create();
+const browserSync = require('browser-sync').create();
+const cleanCSS = require('gulp-clean-css');
+const rename = require('gulp-rename');
 
-gulp.task('hello', function(done) {
+gulp.task('hello', (cb) => {
   console.log('Привет мир!');
-  done();
+  cb();
 });
 
 // Static server
-gulp.task('browser-sync', function () {
+gulp.task('browser-sync', () => {
   browserSync.init({
     server: {
       baseDir: "./"
     }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('minify-css', () => {
+  return gulp.src('css/*.css')
+    .pipe(cleanCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('dist'));
 });
