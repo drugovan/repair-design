774f37866d187d060279506259a9920ccb06a70d
Nastya Drugova <drugova723@gmail.com>
2020-05-14 13:39:33 +0300

    feat: added closing modal window

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   const modal = document.querySelector('.modal')
   const modalBtn = document.querySelectorAll('[data-toggle=modal]')
   const closeBtn = document.querySelector('.modal__close')
+  const modalDialog = document.querySelector('.modal__dialog')
   const switchModal = () => {
     modal.classList.toggle('modal--visible');
   }
@@ -11,4 +12,17 @@ document.addEventListener("DOMContentLoaded", function (event) {
   });
 
   closeBtn.addEventListener('click', switchModal);
+
+  document.addEventListener('keyup', (e) => {
+    if (e.keyCode == 27) {
+      switchModal();
+    }
+  });
+
+  modal.addEventListener('click', (e) => {
+    if (e.target == modalDialog.parentNode) {
+      switchModal();
+    }
+  });
+
 });
